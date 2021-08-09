489f48b679d4b94b9b717d516e57e3b8fd44f14b
Gal Caspi <caspigal7@gmail.com>
2021-08-09 16:57:39 +0300

    Header change

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,16 +12,17 @@ const Header = () => {
         variant="dark"
       >
         <Container>
-          <Navbar.Brand style={{ fontSize: 23 }}>
-            ניר ארגוב - מורה דרך
+          <Navbar.Brand style={{ fontSize: 20 }}>
+            ד"ר ניר ארגוב - מורה דרך
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text style={{ fontSize: 10 }}>
-              Email: <a href="#email">nirar2012@gmail.com</a> Phone:{" "}
-              <a href="#phone">052-3703240 </a>{" "}
-            </Navbar.Text>
-          </Navbar.Collapse>
+          <Navbar
+            aria-controls="responsive-navbar-nav"
+            className="justify-content-end"
+          />
+          <Navbar.Text style={{ fontSize: 11 }}>
+            Email: <a href="#email">nirar2012@gmail.com</a> Phone:{" "}
+            <a href="#phone">052-3703240 </a>{" "}
+          </Navbar.Text>
         </Container>
       </Navbar>
     </>
